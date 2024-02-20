b553acc5b480024bd99c1e5809bce6e69f9e7690
rayague03 <rayague03@gmail.com>
2024-02-20 15:51:35 +0100

    status agent dispo

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -12,9 +12,12 @@ class SearchController extends Controller
     public function index()
     {
         $agents = User::where('role', 'agent')
-                    ->leftJoin('user_statuses', 'users.id', '=', 'user_statuses.user_id')
-                    ->select('users.*', 'user_statuses.status')
-                    ->get();
+    ->leftJoin('user_statuses', 'users.id', '=', 'user_statuses.user_id')
+    ->where('user_statuses.status', 'available') // Filtrer les agents disponibles
+    ->select('users.*', 'user_statuses.status')
+    ->get();
+
         return view('searchagent', compact('agents'));
     }
+
 }
